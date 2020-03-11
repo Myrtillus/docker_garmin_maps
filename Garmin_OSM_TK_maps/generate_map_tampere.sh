f1781f8d19976de1bcb902421a608a902a1d4cf3
Antti Nissinen <antti@myrtillus.net>
2020-03-11 23:31:09 +0200

    osm data ladataan kahdessa setissa ja kootaan yhteen ennen splitteriin ajoa

--- Garmin_OSM_TK_maps/generate_map_tampere.sh
+++ Garmin_OSM_TK_maps/generate_map_tampere.sh
@@ -2,8 +2,6 @@
 
 cd /Garmin_OSM_TK_maps
 
-# Update the style files before generation................
-#sudo -u nissiant git pull
 
 # TAMPERE
 #################
@@ -19,14 +17,16 @@ cd /Garmin_OSM_TK_maps
 #wget -O tampere.osm "http://z.overpass-api.de/api/xapi_meta?*[bbox=      22.8, 61.05, 25, 61.7881]"
 
 
-wget --user-agent Mozilla/5.0 --header="Host: kartta.arpotechno.fi" --header="Referer: http://kartta.arpotechno.fi (antti@myrtillus.net)" -O tampere.osm "http://www.overpass-api.de/api/xapi_meta?*[bbox=22.8,61.05,25,61.7881]"
 
-#wget --user-agent Mozilla/5.0 --header="Host: kartta.arpotechno.fi" --header="Referer: http://kartta.arpotechno.fi (antti@myrtillus.net)" -O tampere.osm "http://overpass.osm.rambler.ru/cgi/xapi_meta?*[bbox=22.8,61.05,25,61.7881]"
+# pohjoinen tampere
+wget --user-agent Mozilla/5.0 --header="Host: kartta.arpotechno.fi" --header="Referer: http://kartta.arpotechno.fi (antti@myrtillus.net)" -O tampere_pohjoinen.osm "http://www.overpass-api.de/api/xapi_meta?*[bbox=22.8 , 61.5 , 25 , 62]"
 
+# etelainen tampere
+wget --user-agent Mozilla/5.0 --header="Host: kartta.arpotechno.fi" --header="Referer: http://kartta.arpotechno.fi (antti@myrtillus.net)" -O tampere_etelainen.osm "http://www.overpass-api.de/api/xapi_meta?*[bbox=22.8 , 61 , 25 , 61.5]"
 
-#wget --user-agent Mozilla/5.0 --header="Host: kartta.arpotechno.fi" --header="Referer: http://kartta.arpotechno.fi (antti@myrtillus.net)" -O tampere.osm "http://z.overpass-api.de/api/xapi_meta?*[bbox=22.8,61.05,25,61.7881]"
+# yhdistetaan osm fileet
+osmosis --rx tampere_pohjoinen.osm --rx tampere_etelainen.osm  --merge --wx tampere.osm
 
-sleep 20
 
 
 # Split the osm file to smaller pieces
@@ -35,7 +35,7 @@ java -jar -Xmx1000m splitter.jar tampere.osm\
  --description="Tampere Region"\
  --precomp-sea=sea.zip\
  --geonames-file=cities15000.zip\
- --max-areas=1024\
+ --max-areas=4096\
  --max-nodes=1000000\
  --mapid=48730001\
  --status-freq=2\
@@ -48,9 +48,6 @@ python3 fix_names.py TK_MTB_Tampere
 
 
 # Create the gmapsupp map file, NOTE THE MAPNAME HAS TO UNIQUE, FAMILY ID IS ALSO UNIQUE
-
-# kesa v√versio kartasta
-#java -jar -Xmx1000m mkgmap.jar --max-jobs --gmapsupp --latin1 --tdbfile --mapname=88880001 --description="OpenStreetMap-pohjainen MTB-kartta, Tampereen alue" --family-id=8888 --series-name="OSM MTB Suomi" --style-file=TK/ TK_Tampere.typ --cycle-map --precomp-sea=sea.zip --generate-sea --bounds=bounds.zip --remove-ovm-work-files -c template.args 
 
 java -jar -Xmx1000m mkgmap.jar\
  --max-jobs\
@@ -69,9 +66,6 @@ java -jar -Xmx1000m mkgmap.jar\
  --remove-ovm-work-files\
  -c template.args 
 
-
-# talviversio kartasta
-#java -jar -Xmx1000m mkgmap.jar --max-jobs --gmapsupp --latin1 --tdbfile --mapname=88880001 --description="OpenStreetMap-pohjainen MTB-kartta, Tampereen alue" --family-id=8888 --series-name="OSM MTB Suomi" --style-file=TK_W/ TK_Tampere.typ --cycle-map --precomp-sea=sea.zip --generate-sea --bounds=bounds.zip --remove-ovm-work-files -c template.args
 
 
 # copy the map file to folder that is mapped to local host
